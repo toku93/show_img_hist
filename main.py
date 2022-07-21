ea27e6fe04791f4f7ceae32c109339d13837ce5c
Tatsuki Tokumura <cmj14086@ict.nitech.ac.jp>
2022-07-21 18:52:27 +0900

    change image url

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from skimage.io import imread
 
 
 # download the image
-img_url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Mount_Fuji_from_Mount_Aino.jpg/640px-Mount_Fuji_from_Mount_Aino.jpg'
+img_url = 'https://pakutaso.cdn.rabify.me/shared/img/thumb/shikun20220402_114719-2.jpg.webp?d=350'
 
 im = imread(img_url)
 
